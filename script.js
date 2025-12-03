92ac30777ba65057fd10e74dccd4e4cf18cbd41c
sasha1809125 <zeleninsasha2012@icloud.com>
2025-12-03 21:49:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,63 +1,15 @@
-let mining = false;
-let interval;
-let timeout;
+// Boot animation
+setTimeout(() => {
+    document.getElementById("boot-screen").classList.add("hidden");
+    document.getElementById("desktop").classList.remove("hidden");
+}, 2500); // 2.5 sec boot, как у старых маков
 
-let target = random8();
-let balance = 0;
+// Open window on HD click
+document.getElementById("icon-hd").addEventListener("dblclick", () => {
+    document.getElementById("window").classList.remove("hidden");
+});
 
-document.getElementById("target").innerText = target;
-
-function random8() {
-    return Math.floor(10000000 + Math.random() * 90000000).toString();
-}
-
-document.getElementById("startBtn").onclick = () => {
-    if (mining) return;
-
-    mining = true;
-
-    interval = setInterval(() => {
-        let n = Math.floor(Math.random() * 100000000)
-                .toString().padStart(8, "0");
-        document.getElementById("try").innerText = n;
-    }, 30);
-
-    let time = 300000 + Math.random() * 300000;
-
-    timeout = setTimeout(() => {
-        mining = false;
-        clearInterval(interval);
-
-        balance += 10;
-        document.getElementById("balance").innerText = balance;
-
-        target = random8();
-        document.getElementById("target").innerText = target;
-    }, time);
-};
-
-document.getElementById("withdrawBtn").onclick = () => {
-    const shillings = balance * 240;
-
-    document.getElementById("modalText").innerHTML =
-        `Вы вывели <b>${balance} CHC</b><br>
-         Это <b>${shillings}</b> шиллингов`;
-
-    balance = 0;
-    document.getElementById("balance").innerText = balance;
-
-    document.getElementById("modal").style.display = "block";
-};
-
-function closeModal() {
-    document.getElementById("modal").style.display = "none";
-}
-
-document.getElementById("modeBtn").onclick = () => {
-    document.body.classList.toggle("day");
-
-    if (document.body.classList.contains("day"))
-        document.getElementById("modeBtn").innerText = "Тёмный режим";
-    else
-        document.getElementById("modeBtn").innerText = "Светлый режим";
-};
+// Close window
+document.getElementById("close-window").addEventListener("click", () => {
+    document.getElementById("window").classList.add("hidden");
+});
